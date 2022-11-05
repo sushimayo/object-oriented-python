e938b66a6b4fc84584d55e1532f71c59a208d564
sushimayo <osushimayo@gmail.com>
2022-11-05 16:05:45 +0900

    add: HTMLテンプレートの使用

---
+++ templating/tutorial/views.py
@@ -0,0 +1,10 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+
+@view_config(route_name='home', renderer='home.pt')
+def home(request):
+    return {'name': 'Home View'}
+
+@view_config(route_name='hello', renderer='home.pt')
+def hello(request):
+    return {'name': 'Hello View'}
