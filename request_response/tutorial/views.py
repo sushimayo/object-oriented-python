0149dec3e8dfdc922cb9cc833be272394de6a001
sushimayo <osushimayo@gmail.com>
2022-11-05 16:25:01 +0900

    add: httpリクエストとレスポンス

---
+++ request_response/tutorial/views.py
@@ -0,0 +1,21 @@
+from pyramid.httpexceptions import HTTPFound
+from pyramid.response import Response
+from pyramid.view import view_config
+
+class TutorialViews:
+    def __init__(self, request):
+        self.request = request
+
+    @view_config(route_name='home')
+    def home(self):
+        return HTTPFound(location='/plain')
+
+    @view_config(route_name='plain')
+    def plain(self):
+        name = self.request.params.get('name', 'No name provided.')
+
+        body = 'URL %s with name: %s' % (self.request.url, name)
+        return Response(
+            content_type='text/plain',
+            body=body
+        )
